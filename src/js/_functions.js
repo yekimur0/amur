e44616272608173f763b80523d65dc93a33eeeee
jay ql <ruslanpavlov072@gmail.com>
2024-09-15 02:57:52 +1000

    a

--- src/js/_functions.js
+++ src/js/_functions.js
@@ -20,11 +20,9 @@ import { mobileCheck } from "./functions/mobile-check";
 import { burger } from "./functions/burger";
 
 // Реализация остановки скролла (не забудьте вызвать функцию)
-// import { disableScroll } from "./functions/disable-scroll";
-
+import { disableScroll } from "./functions/disable-scroll";
 // Реализация включения скролла (не забудьте вызвать функцию)
-// import { enableScroll } from "./functions/enable-scroll";
-
+import { enableScroll } from "./functions/enable-Scroll";
 // Реализация модального окна
 // import GraphModal from 'graph-modal';
 // const modal = new GraphModal();
